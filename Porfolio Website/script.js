e0d43bb17a5e31e8dbb6af53f949b0b2422550b7
xeno <xenoglorp@gmail.com>
2024-09-10 11:10:43 -0400

    Delete Porfolio Website directory
    this was the entire folder pasted into github making it more difficult to access the code

--- Porfolio Website/script.js
+++
@@ -1,9 +0,0 @@
-function toggleMenu(){
-    const menu = document.querySelector(".menu-links");
-    const icon = document.querySelector(".hamburger-icon");
-
-
-    // when we click it will apply or remove the 'open' class in css
-    menu.classList.toggle("open");
-    icon.classList.toggle("open");
-}
